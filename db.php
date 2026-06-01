24d8d964dcf6eead09b2b4dd5ed210b1d8d7703b
RizqiAulia23 <aulnpc34@gmail.com>
2026-06-01 21:52:24 +0800

    Update database config

--- db.php
+++ db.php
@@ -1,8 +1,15 @@
 <?php
-$hostname = 'localhost';
-$username = 'root';
-$password = '';
-$dbname   = 'azure';
 
-$conn = mysqli_connect($hostname, $username, $password, $dbname) or die ('Gagal terhubung ke database');
+$hostname = $_ENV['MYSQLHOST'];
+$username = $_ENV['MYSQLUSER'];
+$password = $_ENV['MYSQLPASSWORD'];
+$dbname   = $_ENV['MYSQLDATABASE'];
+
+$conn = mysqli_connect(
+    $hostname,
+    $username,
+    $password,
+    $dbname
+) or die('Gagal terhubung ke database');
+
 ?>
